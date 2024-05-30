a912761f1e39a518d4e12232ffd5ba431b663756
Brela <claybreland1@gmail.com>
2024-05-30 09:35:36 -0500

    fix cors origins

--- src/middleware/CorsMiddleware.ts
+++ src/middleware/CorsMiddleware.ts
@@ -1,6 +1,16 @@
 import cors from "cors";
 
+const allowedOrigins = ["http://localhost:3000", "https://brela-exams-front.vercel.app/"];
+
 export default cors({
-  origin: "*",
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 });
